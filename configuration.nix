62f9cabd56e24aee7fe26af06bb890ef330e9ecb
RazeLighter777 <gorgonballs@proton.me>
2025-06-09 10:24:03 -0400

    fixing config

--- configuration.nix
+++ configuration.nix
@@ -78,8 +78,8 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
