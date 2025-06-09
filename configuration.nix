cde36e6d0bbe2406d44f9e05e496731b18a4e9c2
RazeLighter777 <gorgonballs@proton.me>
2025-06-09 13:47:47 +0000

    nxi modle

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,9 @@
     git
   ];
 
+  modules = [ arion.nixosModules.arion ];
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
