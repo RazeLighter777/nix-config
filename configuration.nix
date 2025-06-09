49efa41c536bb14cf88d9fc73635414da68a5906
RazeLighter777 <gorgonballs@proton.me>
2025-06-09 10:23:32 -0400

    fixing config

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,7 @@
   hardware.graphics = {
     enable = true;
   };
-  services.xserver.videoDrivers = ["nvidia"]
+  services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
