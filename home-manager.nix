b524c1e912543597f9296cae3301ec19d6a5b5fb
RazeLighter777 <gorgonballs@proton.me>
2025-06-09 21:29:00 +0000

    firefox config

--- home-manager.nix
+++ home-manager.nix
@@ -11,7 +11,61 @@ in
   home-manager.users.justin = { pkgs, ... }: {
     home.packages = [ pkgs.atool pkgs.httpie ];
     programs.bash.enable = true;
-    programs.firefox.enable = true;
+    programs.firefox = {
+      enable = true;
+      profiles.default = {
+        id = 0;
+
+        userChrome = ''
+          toolbar#nav-bar, nav-bar-customization-target {
+            background: ${thm.base00} !important;
+          }
+        '';
+
+        settings = {
+          "extensions.autoDisableScopes" = 0;
+
+          "browser.search.defaultenginename" = "Google";
+          "browser.search.selectedEngine" = "Google";
+          "browser.urlbar.placeholderName" = "Google";
+          "browser.search.region" = "US";
+
+          "browser.uidensity" = 1;
+          "browser.search.openintab" = true;
+          "xpinstall.signatures.required" = false;
+
+          "font.name.monospace.x-western" = "${fonts.mono.family}";
+          "font.name.sans-serif.x-western" = "${fonts.main.family}";
+          "font.name.serif.x-western" = "${fonts.serif.family}";
+
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+
+          "experiments.activeExperiment" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
+          "network.allow-experiments" = false;
+
+          "extensions.pocket.enabled" = false;
+
+          "places.history.enabled" = false;
+          "browser.sessionstore.resume_from_crash" = false;
+          "browser.sessionstore.max_tabs_undo" = 0;
+
+        };
+      };
+    }
     programs.home-manager.enable = true;
     programs.git = {
       enable = true;
