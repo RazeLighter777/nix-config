39d60cdd7b70b42ba58e20a96e4eb7e248fb77ca
RazeLighter777 <gorgonballs@proton.me>
2025-06-09 17:53:40 -0400

    removing errored shadow:

--- home-manager.nix
+++ home-manager.nix
@@ -80,10 +80,6 @@ in
     };
     programs.wofi.enable = true;
     wayland.windowManager.hyprland.settings = {
-      decoration = {
-        shadow_offset = "0 5";
-        "col.shadow" = "rgba(00000099)";
-      };
 
       "$mod" = "SUPER";
 
