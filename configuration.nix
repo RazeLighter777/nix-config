2f38d2f31e16e8296d4215027b818c83b41d26c7
RazeLighter777 <gorgonballs@proton.me>
2025-06-10 16:58:17 -0400

    unfree

--- configuration.nix
+++ configuration.nix
@@ -90,7 +90,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = (pkg: true);
+  nixpkgs.config.allowUnfreePredicate = (_: true);
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
