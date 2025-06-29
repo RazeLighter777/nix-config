e754dd715c25e35711211d6dd9ec72f878a0771c
RazeLighter777 <gorgonballs@proton.me>
2025-06-29 14:39:07 -0400

    fixing

--- modules/home-manager/home-manager.nix
+++ modules/home-manager/home-manager.nix
@@ -103,7 +103,7 @@ in
     };
     programs.vscode = {
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
+      extensions = with pkgs.vscode-extensions [
         dracula-theme.theme-dracula
         yzhang.markdown-all-in-one
         github.copilot
