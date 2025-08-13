6c3b45d4b73fc12ffe951a87d22e5539fd14121a
RazeLighter777 <gorgonballs@proton.me>
2025-08-13 09:17:01 -0400

    array

--- modules/xmrig.nix
+++ modules/xmrig.nix
@@ -1,17 +1,34 @@
-{ pkgs, ... }:
-
+{ pkgs, lib, ... }:
 {
   services.xmrig = {
     enable = true;
-    autosave = true;
-    cpu = true;
-    opencl = false;
-    cuda = false;
-    pools = {
-      url = "pool.supportxmr.com:443";
-      user = "89up8rXVsKi89jpydFLUFc9ZY7mG7Aau21MJKaMgBSZe7Ea4jogQKMUJd4hfCHwr4Z8wvUpbRPiRHMUW5ppdUWFwFLd5Bsm";
-      keepalive = true;
-      tls = true;
+    package = pkgs.xmrig-mo;
+    settings = {
+      autosave = true;
+      randomx = {
+        "1gb-pages" = true;
+      };
+      http = {
+        enabled = true;
+        port = 1984;
+        host = "127.0.0.1";
+        access_token = null;
+        restricted = true;
+      };
+
+      cpu = {
+        enabled = true;
+      };
+      opencl = false;
+      cuda = false;
+      pools = [
+        {
+          url = "pool.supportxmr.com:3333";
+          user = "89up8rXVsKi89jpydFLUFc9ZY7mG7Aau21MJKaMgBSZe7Ea4jogQKMUJd4hfCHwr4Z8wvUpbRPiRHMUW5ppdUWFwFLd5Bsm";
+          keepalive = true;
+          tls = true;
+        }
+      ];
     };
   };
 }
