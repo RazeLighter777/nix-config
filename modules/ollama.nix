e2ddad62b884488dc0f0b4b036cef3b2b8cbb104
RazeLighter777 <gorgonballs@proton.me>
2025-08-16 20:44:18 -0400

    ollama

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,10 +1,24 @@
 { pkgs, lib, ... }:
+let
+  customOllama = pkgs.ollama-cuda.overrideAttrs (old: {
+    src = pkgs.fetchFromGitHub {
+      owner = "ollama";
+      repo = "ollama";
+      rev = "d925b5350c75a66e7830e00f53b243084395821f";
+      sha256 = "sha256-/uo35G5aWyU/TBPeaCA1muw2hZgOokONW29Ox9vZgg4=";
+    };
+  });
+in
 {
   services.ollama = {
     enable = true;
+    package = customOllama;
     loadModels = [
-      "hf.co/unsloth/GLM-4.5-Air-GGUF:Q4_K_M"
+      # "gpt-oss:120b"
     ];
     acceleration = "cuda";
   };
+  environment.systemPackages = with pkgs; [
+    customOllama
+  ];
 }
