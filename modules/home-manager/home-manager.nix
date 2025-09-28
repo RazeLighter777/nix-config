be8de38c29f527f11ac0fb5a059f1128bbeccd98
RazeLighter777 <gorgonballs@proton.me>
2025-09-28 17:46:15 -0400

    home

--- modules/home-manager/home-manager.nix
+++ modules/home-manager/home-manager.nix
@@ -24,9 +24,9 @@ in
         "${
           fetchTarball {
             url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-            sha256 = "1l77kybmghws3y834b1agb69vs6h4l746ga5xccvz4p1y8wc67h7";
+            sha256 = "1rdn70jrg5mxmkkrpy2xk8lydmlc707sk0zb35426v1yxxka10by";
           }
-        }/modules/vscode-server/home.nix"
+        }/modules/ver/home.nix"
       ];
       # User-specific settings (packages, stateVersion, etc.)
       home.username = "justin";
