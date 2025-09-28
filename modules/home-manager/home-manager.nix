b5326b000820860c9d0ead222930e6d5cd92183d
RazeLighter777 <gorgonballs@proton.me>
2025-09-28 17:52:14 -0400

    vscode

--- modules/home-manager/home-manager.nix
+++ modules/home-manager/home-manager.nix
@@ -39,7 +39,7 @@ in
         allowUnfree = true;
       };
       # Add any other user-specific settings here
-      home.stateVersion = "25.05"
+      home.stateVersion = "25.05";
     };
   home-manager.extraSpecialArgs = { inherit unstable; };
 }
