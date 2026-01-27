3eb978df8a45834a7c1fba474a8ae71730f1e4e5
justin <utilityemal77@gmail.com>
2026-01-27 08:11:47 -0500

    waybar battery

--- modules/waybar/default.nix
+++ modules/waybar/default.nix
@@ -137,6 +137,24 @@ in
               "tray"
             ];
 
+            "battery" = {
+              "states" = {
+                "good" = 95;
+                "warning" = 30;
+                "critical" = 15;
+              };
+              "format" = "{icon} {capacity}%";
+              "format-charging" = " {capacity}%";
+              "format-plugged" = " {capacity}%";
+              "format-alt" = "{icon} {time}";
+              "format-icons" = [
+                ""
+                ""
+                ""
+                ""
+                ""
+              ];
+            };
             network = {
               interval = 1;
               format-disconnected = "Disconnected ⚠";
