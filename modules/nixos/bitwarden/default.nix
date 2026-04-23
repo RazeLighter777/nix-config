0ce63a901b97c8d1726fbd9090283ab83d214548
Justin Suess <utilityemal77@gmail.com>
2026-04-23 12:32:53 -0400

    removed bitwarden

--- modules/nixos/bitwarden/default.nix
+++ modules/nixos/bitwarden/default.nix
@@ -8,8 +8,6 @@
   config = lib.mkIf config.my.bitwarden.enable {
     nixpkgs.config.allowUnfree = lib.mkDefault true;
     environment.systemPackages = [
-      pkgs.bitwarden-cli
-      pkgs.bitwarden-desktop
     ];
   };
 }
